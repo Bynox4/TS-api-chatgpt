637b9d4195fd1230859689ee021b7003b77bafc4
Nelson Sanchez <bynoxsi2017@gmail.com>
2023-05-27 15:34:52 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const button = document.querySelector('button');
+const textarea = document.querySelector('textarea');
+
+export const llamarApi = async( msg:string ) => {
+    const configuration = new Configuration({
+      apiKey: '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'
+  });
+
+    delete configuration.baseOptions.headers['User-Agent'];
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: `Mejora este texto: ${msg}`}],
+});
+    textarea!.value = response.data.choices[0].message?.content || 'ERROR';
+  }
+
+
+button!.addEventListener('click', async() => {
+  if ( textarea!.value.trim().length !== 0 ){
+    await llamarApi( textarea!.value );
+  }
+});
